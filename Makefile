79f5d5a3bc335fa5a64b714b549d4287380bbb41
Joe Moger <jmoger@uvic.ca>
2026-01-28 14:46:45 -0800

    updated makefile

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
 .phony all:
-all: pman
+all: pman test inf
 
 pman: main.c linked_list.c
 	gcc -Wall main.c linked_list.c -o pman
 
+test: test.c 
+	gcc -Wall test.c -o test
+
+inf: inf.c
+	gcc -Wall inf.c -o inf
+
+args: args.c
+	gcc -Wall args.c -o args
+
 .PHONY clean:
 clean:
-	-rm -rf *.o *.exe
+	-rm -f pman test inf args *.o *.exe
--- test.c
+++ test.c
@@ -1,12 +1,22 @@
-#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 int main() {
     int c = 0;
     while (1) {
-        c++;
-        printf("%d\n", c);
-        sleep(5);
+        c++;                                       // increase utime
+        // printf("%d\n", c);
+        // sleep(5);
+        int status;
+        int value = waitpid(-1, &status, WNOHANG); // system call to increase stime
     }
     return 0;
 }
